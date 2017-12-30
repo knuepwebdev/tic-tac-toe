fe5329f22e590aa2ffbee10eac064e4386daa62b
Mike <mikeknueppel@hotmail.com>
2017-12-30 14:47:46 -0800

    Lift state up into the Board

--- src/index.js
+++ src/index.js
@@ -2,26 +2,33 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+// Square is an example of a "controlled component" - its state is managed by Board, it's parent
 class Square extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: null,
-    };
-  }
-
   render() {
     return (
-      <button className="square" onClick={() => this.setState({value: 'X'})}>
-        {this.state.value}
+      <button className="square" onClick={this.props.onClick}>
+        {this.props.value}
       </button>
     );
   }
 }
 
 class Board extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      squares: Array(9).fill(null)
+    }
+  }
+
   renderSquare(i) {
-    return <Square value={i} />;
+    return <Square value={this.state.squares[i]} onClick={ () => this.handleClick(i) } />;
+  }
+
+  handleClick(i) {
+    const squares = this.state.squares.slice()
+    squares[i] = 'X'
+    this.setState({ squares: squares })
   }
 
   render() {
